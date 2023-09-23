979eecbc9b89e8738d0d170696cf2467bb0328c3
Jake Marson Nable <Jake Marson Nable@LAPTOP-B8AKVODA>
2023-09-23 13:38:26 +0800

    Quiz2

---
+++ src/nable_quiz2/Nable_Quiz2.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package nable_quiz2;
+
+/**
+ *
+ * @author Jake Marson Nable
+ */
+public class Nable_Quiz2 {
+
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
